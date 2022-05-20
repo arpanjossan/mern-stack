e583067f37aa1fb85390c98d5a8a7cd7cdcbedb4
arpanjossan <gurarpanpreet.singh@antiersolutions.com>
2022-05-20 17:57:39 +0530

    server_setup

---
+++ backend/app.js
@@ -0,0 +1,9 @@
+const express = require("express")
+const app = express();
+app.use(express.json())
+
+// Router Imports
+
+const product = require("./routes/productRoutes")
+app.use("/api/v1" , product)
+module.exports = app
---
+++ backend/server.js
@@ -0,0 +1,16 @@
+const app = require("./app")
+
+const dotenv = require("dotenv")
+
+dotenv.config({path:"backend/config/config.env"})
+// app.listen(process.env.PORT,()=>{
+
+//     console.log(`Server is working on http://localhost:${process.env.PORT}`);
+// })
+// const express = require("express");
+// const app = express();
+
+const PORT = 3004
+app.listen(PORT , ()=>{
+    console.log("start" , PORT);
+})
